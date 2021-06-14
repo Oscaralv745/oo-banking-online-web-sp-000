170db4ab3e82e3c80cb3e004e61ed3b4229a1e1f
Oscar Alvarez <gavinalv6@gmail.com>
2021-06-14 20:23:32 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,7 @@
 class BankAccount
   attr_reader :name, :balance, :status
 
+
   def initialize(name)
     @name = name
     @balance = 1000
@@ -8,6 +9,6 @@ class BankAccount
   end
 
   def deposit(money)
-
+    
   end
 end
