bdd6f0f74d4c9c8e2880850d8de912e9603b1eb5
Oscar Alvarez <gavinalv6@gmail.com>
2021-06-14 20:37:25 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,7 +17,10 @@ class BankAccount
   end
 
   def valid?
-    if @status = "open" && @balance > 0 ? true : false
+    if @status == "open" && @balance > 0
+      true 
+    else 
+      false
     end 
   end
 end
