16ce01f7904d4e4a86d421e0b7f2085f6ea5e005
Oscar Alvarez <gavinalv6@gmail.com>
2021-06-14 20:46:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,11 @@
 class Transfer
   attr_reader :transfer, :sender, :receiver
-  attr_accessor :status
+  attr_accessor :status, :amount
 
-  def initialize(sender, receiver, transfer)
+  def initialize(sender, receiver, amount, transfer)
     @sender = sender
     @receiver = receiver
+    @amount = amount
     @transfer = transfer
     @status = "pending"
   end
